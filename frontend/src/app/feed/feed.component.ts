fdfe46aa496d5cd2a74b3f5ca8e5e42e9c32e887
Daniel Sparber <daniel@sparber.io>
2019-12-13 15:45:42 -0500

    Added button to load more posts

--- frontend/src/app/feed/feed.component.ts
+++ frontend/src/app/feed/feed.component.ts
@@ -9,7 +9,10 @@ import { FeedService } from './service/feed.service'
 export class FeedComponent implements OnInit, OnChanges  {
 
   @Input() wall:String = null;
-  postIds:any = [];
+  postIds:any = null;
+  delta:number = 5;
+  maxPosts:number = 5;
+
 
   constructor(private feedService: FeedService) { }
 
@@ -18,11 +21,17 @@ export class FeedComponent implements OnInit, OnChanges  {
   }
 
   ngOnChanges() {
+    this.maxPosts = 5;
+    this.postIds = [];
     this.getFeed();
   }
 
   insertPost(post) {
     this.postIds.splice(0, 0, post.postId);
+  }
+
+  showMore() {
+    this.maxPosts += this.delta;
   }
 
   onScroll(event) {
