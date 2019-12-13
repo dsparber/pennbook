3068ad4a23e700ed7edf85b02476d5701eb51cda
Daniel Sparber <daniel@sparber.io>
2019-12-12 20:01:01 -0500

    Fixes

--- backend/db/database.js
+++ backend/db/database.js
@@ -377,7 +377,6 @@ async function posts(walls, callback) {
     try {
         let posts = await getItems(db.Post.scan().where('wall').in(walls).where('parent').null().exec());
         posts = posts.sort((a, b) => a.createdAt < b.createdAt ? 1 : -1);
-        posts = [posts[0]]; /// TEST 
         posts = await Promise.all(posts.map(async post => await mapPost(post)));
         callback(null, posts);
         return posts;
